c4b415969982b1dc4bf6d782f9e25c1de0f19428
denis <denis@debian>
2020-07-02 12:11:36 +0200

    Test apps.js client with afterEach

--- test/client/containers/app.js
+++ test/client/containers/app.js
@@ -1,70 +1,133 @@
-// import React from 'react';
-// import App from '../../../src/client/containers/app';
-// import { shallow, render } from 'enzyme';
-// import { Provider } from 'react-redux';
-// import { configureStore } from '../../helpers/server'
-// import rootReducer from '../../../src/client/reducers'
-// import { startServer } from '../../helpers/server'
-// import params from '../../../params';
-// // describe('Check App', () => {
-// //   it('Test App', (done) => {
+import React from 'react';
+import App from '../../../src/client/containers/app';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { configureStore } from '../../helpers/server'
+import rootReducer from '../../../src/client/reducers'
+import { startServer } from '../../helpers/server'
+import params from '../../../params';
+import io from 'socket.io-client';
+import { mount } from 'enzyme';
+import { StaticRouter } from 'react-router-dom';
+import { initialBoard } from '../../../src/client/gameHelpers';
 
-// //     done();
-// //   });
-// // })
+describe('App Component', () => {
+  let initialState = {}
+  let store = {};
+  const context = {};
+  let paramsTest = params;
+  paramsTest.server.port = 3007;
+  let socket = io(paramsTest.server.url);
+  let location = '';
+  let error = false;
 
+  afterEach(() => {
+    const wrapper = mount(
 
-// describe('App Component', () => {
+      <Provider store={store}>
+        <StaticRouter location={location} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    )
 
-//   let tetrisServer;
-//   let socket;
+    if (error)
+      wrapper.find('.error').simulate('click');
+      error = false;
+  });
 
-//   let paramsTest = params;
-//   paramsTest.server.port = 8080;
-//   before(cb => startServer(paramsTest.server, function (err, server) {
-//     tetrisServer = server
-//     socket = io(paramsTest.server.url);
-//     cb()
-//   }))
+  it('Render the App', (done) => {
+    initialState = {
+      socket: socket,
+      player: {
+        delay: null,
+        socketId: socket.id,
+        pieces: [
+          [
+            [4, 4],
+            [4, 4],
+          ],
+          [
+            [2, 0, 0],
+            [2, 2, 2],
+            [0, 0, 0],
+          ],
+          [
+            [0, 0, 3],
+            [3, 3, 3],
+            [0, 0, 0],
+          ],
+        ],
+        piece: {
+          tetromino: [
+            [0, 6, 0],
+            [6, 6, 6],
+            [0, 0, 0]
+          ],
+          pos: {
+            x: 5,
+            y: 6
+          },
+          collided: false,
+          new: false
+        }
+      }
+    }
+    store = configureStore(rootReducer, null, initialState);
+    done();
+  });
 
-//   // after(function (done) { tetrisServer.stop(done) })
+  it('renders the App piece new', (done) => {
+    initialState.player.piece.new = true;
+    store = configureStore(rootReducer, null, initialState);
+    done();
+  });
 
-//   it('renders the Counter wrapper', (done) => {
+  it('renders the App piece new and pos y 0', (done) => {
+    initialState.player.piece.pos.y = 0;
+    initialState.player.piece.collided = true;
+    store = configureStore(rootReducer, null, initialState);
+    done();
+  });
 
-//     const initialState = {
-//       player: {
-//         delay: null,
-//         // socketId: socket.id,
-//         // grid: initialBoard(),
-//         // spectre: initialSpectre(),
-//         piece: {
-//           tetromino: [
-//             [0, 6, 0],
-//             [6, 6, 6],
-//             [0, 0, 0]
-//           ],
-//           pos: {
-//             x: 5,
-//             y: 6
-//           },
-//           collided: false,
-//           new: false
-//         }
-//       }
-//     }
-//     const store = configureStore(rootReducer, null, initialState);
-//     const error = render(
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     );
+  it('renders the App hash router', (done) => {
+    location = '/testRoom[testPlayer]';
+    done();
+  });
 
+  it('renders the App click error', (done) => {
+    initialState.error = 'test error';
+    store = configureStore(rootReducer, null, initialState);
+    error = true;
+    done();
+  });
 
-//     done();
-//     // const app = shallow(<App />);
-//     // expect(wrapper.find(Counter)).to.have.length(1);
-//   });
-// });
+  it('renders the App updateTetronimo', (done) => {
+    initialState.player.grid = initialBoard();
+    initialState.player.piece.pos.y = 2;
+    initialState.player.pieces = [
+      [
+        [4, 4],
+        [4, 4],
+      ],
+      [
+        [2, 0, 0],
+        [2, 2, 2],
+        [0, 0, 0],
+      ],
+      [
+        [0, 0, 3],
+        [3, 3, 3],
+        [0, 0, 0],
+      ],
+      [
+        [0, 6, 0],
+        [6, 6, 6],
+        [0, 0, 0],
+      ],
+    ];
+    store = configureStore(rootReducer, null, initialState);
+    done();
+  });
 
-
-
+})

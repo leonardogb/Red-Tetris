c9ad7c66721a2da2c4dc34c68472db6933491689
Leonardo <lgarcia-@student.42.fr>
2021-01-20 15:43:05 +0100

    remove old dependencies

--- params.js
+++ params.js
@@ -1,10 +1,11 @@
 const params = {
   server: {
-    host: 'localhost',
-    port: 3004,
-    get url() { return 'http://' + this.host + ':' + this.port },
+    host: "0.0.0.0",
+    port: process.env.PORT || 3004,
+    get url() {
+      return "https://" + this.host + ":" + this.port;
+    },
   },
 };
 
 module.exports = params;
-
